c36295930e2ea71855b6d9f383ce63555ea70d23
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-12-16 20:24:55 -0200

    finished Type Array

--- src/05 Type Array.ts
+++ src/05 Type Array.ts
@@ -1,6 +1,16 @@
 // Array <T> - T[] posso add o meu proprio tipo
-function mulArgs(...args: Array<number>): number {
+export function mulArgs(...args: Array<number>): number {
     return args.reduce((ac, valor) => ac * valor, 1);
 }
 
+export function concatenaString(...args: string[]): string {
+    return args.reduce((ac, valor) => ac + valor);
+}
+
+export function toUpperCase(...args: string[]): string[] {
+    return args.map((valor) => valor.toUpperCase());
+}
+
 console.log(mulArgs(2,2));
+console.log(concatenaString('a','b', 'c'));
+console.log(toUpperCase('a','b', 'c'));
