4686521f7830f21d7409823b33cae4cce5b4edf8
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-12-16 15:57:16 -0200

    Type void

---
+++ src/03 Type Void.ts
@@ -0,0 +1,16 @@
+// Type void é mais usado nas funções que não retornam nada Ex :
+function semRetorno (...args: string[] ): void{
+    console.log(args.join(', '));
+}
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio',
+    exibirNome(): void {
+        console.log(this.nome, this.sobrenome);
+    }
+}
+semRetorno('Oi', 'Tudo Bem');
+pessoa.exibirNome();
+export { pessoa };
+
+// Dica pessoal : retorno de funções e metodos
