8f26b9a235f8120ddce54f74535818f434d25b87
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-12-16 20:44:49 -0200

    finished type tuple

---
+++ src/06 Type tupla.ts
@@ -0,0 +1,15 @@
+// Tuble
+const dadosClientes: [number, string] = [1, 'Hamilton Silva'];
+const dadosClientes2: [number, string, string] = [1, 'Hamilton Silva', 'Sala 1'];
+dadosClientes.push(1);
+dadosClientes.push(2);
+
+console.log(dadosClientes);
+
+// readonly array
+
+const array1: readonly string[] = ['Hamilton Silva', 'Carlos'];
+const array2: ReadonlyArray<string> = ['a', 'c', 'v'];
+
+
+
