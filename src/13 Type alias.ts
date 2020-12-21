55fc8b75834e5153443357466b2df493b7c32ed3
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-12-21 06:01:37 -0200

    Type alias

---
+++ src/13 Type alias.ts
@@ -0,0 +1,27 @@
+// Criando type elias devem começar com letra maiúscula
+type Idade = number;
+type Pessoa = {
+    nome: string;
+    idade: Idade;
+    salario: number;
+    corPreferida?: string;
+};
+
+type corRGB = 'Vermelho' | 'Verde' | 'Azul';
+type corMik = 'Claro' | 'Magenta' | 'Amarela' | 'Preto';
+type corPreferida = corRGB | corMik;
+
+const pessoa: Pessoa = {
+    nome: 'Hamilton',
+    idade: 19,
+    salario: 100_000,
+    corPreferida:<corPreferida> 'Amarela'
+
+};
+
+export function setCorPreferida(pessoa: Pessoa, cor: corPreferida): Pessoa{
+  return {...pessoa, corPreferida : cor};
+}
+console.log(setCorPreferida(pessoa, 'Preto'));
+
+export default 1;
