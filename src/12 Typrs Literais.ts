6ad719185249be55584864e6f6510eefa4328fcb
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-12-21 05:40:17 -0200

    Typrs Literais

---
+++ src/12 Typrs Literais.ts
@@ -0,0 +1,22 @@
+// Usar valores como type
+
+let x = 10;
+// Type de valor porque o tipo dele só pode ser o valor
+const y = 10;
+let a:100 = 100;
+let c = 100 as const;
+
+const pessoa = {
+    nome: 'Luiz' as const,
+    sobrenome: 'Miranda',
+};
+pessoa.nome = 'Luiz';
+
+function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Azul'): string{
+    return cor;
+}
+const cor: number = escolhaCor('Vermelho').indexOf('V');
+console.log(cor);
+
+// Module mode
+export default 1;
