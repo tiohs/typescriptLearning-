72c27beaf589576649eed168fb17999ea555c5ef
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-02-17 00:35:22 +0100

    terminado

--- src/19 Classes.ts
+++ src/19 Classes.ts
@@ -1,10 +1,35 @@
 export class Empresa {
     public readonly name: String;
-    constructor(name: string) {
+    private readonly colaboradores: Colaborador[] = [];
+    protected readonly cnpj: string;
+
+    constructor(name: string, cnpj: string) {
         this.name = name;
+        this.cnpj = cnpj;
+    }
+    /**
+     * addColaborador
+     */
+    public addColaborador(colaborador: Colaborador): void {
+        this.colaboradores.push(colaborador);
+    }
+    /**
+     * Index colaborador
+     */
+    public Index(): void {
+        this.colaboradores.forEach((a) => {
+            console.log(`${a.firstName} ${a.lastName}`);
+        });
     }
 }
 
-const empresa1 = new Empresa("Uniplace");
-
-console.log(empresa1.name);
+export class Colaborador {
+    constructor(
+        public readonly firstName: string,
+        public readonly lastName: string
+    ) {}
+}
+const empresa1 = new Empresa("Uniplace", "12653-232719-12327");
+const colaborador: Colaborador = new Colaborador("Hamilton", "Silva");
+empresa1.addColaborador(colaborador);
+empresa1.Index();
