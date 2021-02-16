74096c834905c84d01f3e0223c289a0078ef874f
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-02-17 00:17:10 +0100

    Create the primary class

--- src/19 Classes.ts
+++ src/19 Classes.ts
@@ -1,39 +1,10 @@
 export class Empresa {
-    public readonly nome: string; // public não é necessario
-    private readonly colaboradores: Colaborador[] = [];
-    // Ele é const caso eu queira que não seja alterado devo uso em frente do type
-    protected readonly cnpj: string;
-
-    constructor(nome: string, cnpj: string) {
-        this.nome = nome;
-        this.cnpj = cnpj;
-    }
-    addColaborador(colaborador: Colaborador) {
-        this.colaboradores.push(colaborador);
-    }
-    showColaboradore(): void {
-        for (const colaborador of this.colaboradores) {
-            console.log(colaborador);
-        }
+    public readonly name: String;
+    constructor(name: string) {
+        this.name = name;
     }
 }
-export class Colaborador {
-    constructor(
-        public readonly nome: string,
-        public readonly sobrenome: string
-    ) {} // aqui já tem um this
-}
 
-const Empresa1 = new Empresa("Uniplace", "11.115.121/0001-11");
+const empresa1 = new Empresa("Uniplace");
 
-const colaborador = new Colaborador("Hamilton", "Silva");
-const colaborador2 = new Colaborador("Marcio", "Silva");
-const colaborador3 = new Colaborador("Antonia", "Silva");
-
-console.log(Empresa1);
-console.log(Empresa1.nome);
-
-Empresa1.addColaborador(colaborador);
-Empresa1.addColaborador(colaborador2);
-Empresa1.addColaborador(colaborador3);
-Empresa1.addColaborador({ nome: "Osmar", sobrenome: "Andre" });
+console.log(empresa1.name);
