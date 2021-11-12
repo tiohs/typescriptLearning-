db4e5f644267d1d1512169c2eaa32d526857d93a
tiohs <hamiltonsilva551@gmail.com>
2021-11-12 20:54:25 +0100

    Cool

---
+++ src/25 Atributos&MetodosEstaticos.ts
@@ -0,0 +1,25 @@
+
+
+// Só é possivel ter acesso a metodos estaticos pela class melhor dizer sem instaciar
+export class Personal {
+    static ageStart = 0;
+    static cpfStart = '000.000.000-00';
+
+    constructor(
+        public name: string,
+        public lastName: string,
+        public age: number,
+        public cpf: string
+    ){
+
+    }
+    static creatPerson(name: string, lastName: string): Personal {
+       return new Personal(name, lastName, Personal.ageStart, Personal.cpfStart)
+    }
+
+}
+
+const personal = new Personal('Hamilton', 'Silva', 15, '000-000.00');
+console.log(Personal.creatPerson('Paulina', 'Silva'));
+
+
--- src/26 contrutorPrivado.ts
+++ src/26 contrutorPrivado.ts
@@ -1,22 +1,37 @@
 // Crainda apenas uma conexão para a base de dados
 // Nome do metodo Singleton - Gof
 // Ele é muito bom para criar um instacia de um objecto
+// As class podem ser uma tipagem
 export class Database {
-     constructor(
+    private static database: Database;
+     private constructor(
         private host: string,
         private user: string,
-        private password: string
+        private password: string,
         ) {}
         connect(): void {
             console.log(this.host, this.user, this.password);
         }
+
+        static getDatabase( host: string,  user: string, password: string ): Database {
+            if(Database.database) {
+                console.log('Create old instance ');
+                return Database.database;
+            }
+            console.log('Create new instance ');
+            Database.database = new Database(host, user, password);
+            return Database.database;
+        }
 }
 
-const db = new Database('localhost', 'Hamilton Silva', 'massango123');
-const db1 = new Database('localhost', 'Hamilton Silva', 'massango123');
 
-console.log(db);
-console.log(db1);
+const db = Database.getDatabase('localhost', 'tiohs', 'morena');
+const db1 = Database.getDatabase('localhost', 'pedro', 'jessica');
+
+console.log(db === db1)
+
+
+
 
 
 
