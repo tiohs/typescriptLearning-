b85a1a5c4f7b05816c2e35922b6fd0ef4039d1d3
tiohs <hamiltonsilva551@gmail.com>
2022-01-12 09:43:04 +0100

    revision unknown

--- src/10 Type unknown.ts
+++ src/10 Type unknown.ts
@@ -1,6 +1,7 @@
-// Type UNKNOWN Ele é um tipo any mais seguro, ele não funciona sem ser checado qual é o tipo   Ex :
+// Type UNKNOWN Ele é um tipo any mais seguro
+// ele não funciona sem ser checado qual é o tipo em questão  Ex :
 // Posso usar em typagem onde recebe vairos tipos de dados
-
+// No Exemplo asseguir ele é verificado pelo typeof
 let x: unknown;
 
 x = 1;
