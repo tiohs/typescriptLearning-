2061a9b0f20b6d731eced9c18bbf0fe4e845937c
tiohs <hamiltonsilva551@gmail.com>
2022-02-28 14:06:18 +0100

    Materia

---
+++ src/30 Agregação entre classes.ts
@@ -0,0 +1,9 @@
+// Medo de agregação é onde os objectos podem existir sem precisar um do outro
+// Mais não exercem as suas funções sem a existencia de um
+// Por exemplo um carro e as rodas
+// o carro pode existir sem rodas mais precisa agregar as rodas para andar
+// ou fazer o seu metodo inicial começar
+
+class Card {
+
+}
