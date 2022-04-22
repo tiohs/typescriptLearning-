ce7135fc6332e671d14d7c6b73df0108fc80ead6
tiohs <hamiltonsilva551@gmail.com>
2022-04-22 18:53:36 +0100

    import

--- src/Desafio/index.ts
+++ src/Desafio/index.ts
@@ -1,11 +1,2 @@
-import "./form-controllers";
-type thisDados = {
-    name: string;
-};
-function myFunction(this: thisDados, name: string): void {
-    console.log(this);
-    console.log(this.name + name);
-    console.log(name);
-}
-
-myFunction.call({ name: "Hamilton" }, "Massango");
+// import "./form-controllers";
+import "../ExercioVideo/ExercioVideo";
